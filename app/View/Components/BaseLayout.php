be7ef99d232227a7809500d04a8b482e5c6bfdaf
Hridoy Roy <hridayroy05@gmail.com>
2022-01-03 17:09:22 +0600

    Changing the Login and Registration page Layout

--- app/View/Components/BaseLayout.php
+++ app/View/Components/BaseLayout.php
@@ -4,16 +4,15 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class GuestLayout extends Component
+class BaseLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      *
      * @return \Illuminate\View\View
      */
-    
     public function render()
     {
-        return view('layouts.guest');
+        return view('layouts.base');
     }
 }
